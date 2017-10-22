f6e85a8c9bb22158331f58339dee6522ed4676d8
Esteban Padilla <esteban.padilla@fairplaylabs.com>
2017-10-21 18:15:18 -0600

    Saving

--- app.js
+++ app.js
@@ -8,30 +8,4 @@ function init() {
     canvas.height = window.innerHeight;
     let context = canvas.getContext('2d');
     */
-
-    var matilde = new Vaca('Matilde');
-    matilde.comer();
-    matilde.ordennar();
-
-    var juana = new Animal('Juana');
-    juana.comer();
-    juana.setApodo('Juanita');
-    juana.decirApodo();
-
-    var lola = new Animal('Lola');
-    lola.comer();
-    lola.setApodo('Lolita');
-    lola.decirApodo();
-
-    juana.decirApodo();
-
-    console.log(matilde);
-    console.log(juana);
-    console.log(lola);
-
-    console.log(lola.getApodo());
-    lola.removerApodo();
-    console.log(lola.getApodo());
-    //console.log(Object.getOwnPropertyNames(Vaca.prototype));
-
 }
