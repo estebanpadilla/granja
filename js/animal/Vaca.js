3cda146b88d55ce0bdf7aa614e66a1ba3e3cc5c8
Esteban Ruiz <eruizm@ucenfotec.ac.cr>
2017-10-26 18:28:54 -0600

    Test de upload

--- js/animal/Vaca.js
+++ js/animal/Vaca.js
@@ -1,13 +1,11 @@
 var Vaca = (
 	function () {
 		function Vaca(pnombre) {
-
 		}
 		//Class Methods
 		Vaca.prototype.comer = function () {
 			console.log(this.nombre + ' soy una vaca y estoy comiendo.');
 		}
-
 		return Vaca;
 	}
 )();
